d0e46d7e993a8612e2b624745c595cfabc1e97f6
Albrecht Schmidt <mail@albrechtschmidt.de>
2019-12-01 22:12:38 +0100

    groovy solution d1

---
+++ groovy/src/main/groovy/iamdrowsy/aoc/Util.groovy
@@ -0,0 +1,17 @@
+package iamdrowsy.aoc
+
+import java.io.File
+
+class Util {
+
+    static def input(Integer day) {
+        String fileName = String.format('../data/d%02d-input.txt', day);
+        new File(fileName).text;
+    }
+
+    static def check(Integer day, Integer part, String solution) {
+        String fileName = String.format('../data/d%02d-part-%d.txt', day, part);
+        String expected = new File(fileName).text;
+        assert expected == solution
+    }
+}
---
+++ groovy/src/main/groovy/iamdrowsy/aoc/y2019/Day01.groovy
@@ -0,0 +1,34 @@
+package iamdrowsy.aoc.y2019
+import iamdrowsy.aoc.Util
+
+class Day01 {
+
+    static Integer day = 1;
+
+    static Integer neededFuel(Integer weight) {
+        return (weight.intdiv(3)) - 2
+    }
+
+    static String solve1(String input) {
+        input.split().collect{neededFuel(it.toInteger())}.sum();
+    }
+
+    static Integer allNeededFuel(Integer weight) {
+        Integer needed = 0;
+        Integer additional = neededFuel(weight);
+        while (additional > 0) {
+            needed += additional;
+            additional = neededFuel(additional);
+        }
+        return needed;
+    }
+
+    static String solve2(String input) {
+        input.split().collect{allNeededFuel(it.toInteger())}.sum();
+    }
+
+    static void main(String[] args) {
+        Util.check(day, 1, solve1(Util.input(day)));
+        Util.check(day,2,solve2(Util.input(day)));
+    }
+}
