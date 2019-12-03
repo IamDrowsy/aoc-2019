5684afe2752c0f8bf67fff27d8138f1b20501239
Albrecht Schmidt <Albrecht.Schmidt@t-systems.com>
2019-12-03 09:50:48 +0100

    not nice, but working solution

--- clojure/src/advent_of_code/y2019/d03.clj
+++ clojure/src/advent_of_code/y2019/d03.clj
@@ -13,10 +13,10 @@
         dist (parse-long dist-string)
         [curr-x curr-y] (first wire)]
     (reduce conj wire (case dir
-                        "R" (map #(vector % curr-y) (range curr-x (+ curr-x dist)))
-                        "L" (map #(vector % curr-y) (range curr-x (- curr-x dist) -1))
-                        "U" (map #(vector curr-x %) (range curr-y (+ curr-y dist)))
-                        "D" (map #(vector curr-x %) (range curr-y (- curr-y dist) -1))))))
+                        "R" (map #(vector % curr-y) (range (inc curr-x) (inc (+ curr-x dist))))
+                        "L" (map #(vector % curr-y) (range (dec curr-x) (dec (- curr-x dist)) -1))
+                        "U" (map #(vector curr-x %) (range (inc curr-y) (inc (+ curr-y dist))))
+                        "D" (map #(vector curr-x %) (range (dec curr-y) (dec (- curr-y dist)) -1))))))
 
 (defn wire-points [wire-string]
   (reduce extend-wire '([0 0]) (str/split wire-string #",")))
@@ -39,7 +39,7 @@
         [wire1 wire2] (mapv wire-map wire-strings)
         dist-sum (fn [point] (+ (wire1 point) (wire2 point)))
         nearest-point (second (sort-by dist-sum (set/intersection (key-set wire1) (key-set wire2))))]
-    (- (dist-sum nearest-point) 2)))
+    (dist-sum nearest-point)))
 
 
 (defn run []
