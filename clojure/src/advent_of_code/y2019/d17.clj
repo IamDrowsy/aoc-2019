01c09287779752b4ee2e398159572c97c7d823d2
Albrecht Schmidt <mail@albrechtschmidt.de>
2019-12-18 01:22:57 +0100

    d17-1 using neo4j

--- clojure/src/advent_of_code/y2019/d17.clj
+++ clojure/src/advent_of_code/y2019/d17.clj
@@ -4,27 +4,55 @@
     [criterium.core :refer [quick-bench]]
     [clojure.string :as str]
     [clojure.core.async :as a :refer [thread >!! <!! chan]]
-    [advent-of-code.y2019.intcode :as i]))
+    [advent-of-code.y2019.intcode :as i]
+    [neo4j-clj.core :as db]))
 
 (def day 17)
 
-(defn parse-field [input]
-  (let [camera (i/drain (i/start (i/parse-intcode input) []))
-        width (count (first (partition-by #(= 10 %) camera)))]
-    {:field (->> camera
-                 (remove #(= 10 %))
-                 (into []))
-     :width width}))
+(defn node-name [x y]
+  (str "NODE" x "x" y))
 
-(defn neighbors [{:keys [field width]} index]
-  (map field [(inc index) (dec index) (+ index width) (- index width)]))
+(defn nodes [field width height]
+  (str/join ",\n" (for [y (range height)
+                        x (range width)]
+                    (let [obj (field (+ x (* y width)))]
+                      (str/join ",\n"
+                                (remove nil?
+                                        [(str "(" (node-name x y) " {x:" x ", y: " y ", solid: " (= 35 obj) "})")
+                                         (when (< 0 x) (str "(" (node-name x y) ")-[:NEIGHBOR]->(" (node-name (dec x) y) ")"))
+                                         (when (< 0 y) (str "(" (node-name x y) ")-[:NEIGHBOR]->(" (node-name x (dec y)) ")"))]))))))
+
+(defn input->query [input]
+  (let [camera (i/drain (i/start (i/parse-intcode input) []))
+        width (count (first (partition-by #(= 10 %) camera)))
+        field (->> camera (remove #(= 10 %)) (into []))
+        height (/ (count field) width)]
+    (str "CREATE "(nodes field width height))))
+
+(defn fill-db [db input]
+  (db/execute (db/get-session db)
+              (input->query input))
+  db)
+
+(defmacro with-filled-db [input run-fun]
+  `(let [db# (db/create-in-memory-connection)
+         _# (fill-db db# ~input)
+         result# (~run-fun db#)]
+     ((:destroy-fn db#))
+     result#))
+
+(db/defquery cross-section
+             "MATCH (cross {solid:true })--(y {solid: true})
+              WITH cross, count(y) as c, cross.x * cross.y as align
+              WHERE c = 4
+              RETURN sum(align)")
 
 (defn solve-1 [input]
-  (partition-by #(= 10) (i/drain (i/start (i/parse-intcode input) [1]))))
+  (with-filled-db input
+                  (fn [db]
+                    (with-open [session (db/get-session db)]
+                      (val (ffirst (cross-section session)))))))
 
-(defn solve-2 [input]
-  (first (i/drain (i/start (i/parse-intcode input) [2]))))
 
 (defn run []
-  (check day 1 (solve-1 (get-input day)))
-  (check day 2 (solve-2 (get-input day))))
+  (check day 1 (solve-1 (get-input day))))
